28ffd2ee8242930df91691a5d84e30ce1a1a21d9
wilfredo2012 <wmendozam@gmail.com>
2013-04-05 09:17:20 -0400

    primer COMMIT de la rama2

--- Foo.java
+++ Foo.java
@@ -16,3 +16,30 @@ public class Foo {
       this.lastName = lastName;
    }
 }
+
+
+package util;
+public class Foo {
+   private String firstName;
+   private String lastName;
+   private String secondName;
+   public String getFirstName() {
+     return firstName;
+   }
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+   public String getLastName() {
+      return lastName;
+   }
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+}
+   public String getSecondName() {
+      return secondName;
+   }
+   public void setSecondName(String secondName) {
+      this.secondName = secondName;
+   }
+}
